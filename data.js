4f40e59da8081ad4fa16b9674655c1a2ecda099c
Kate Greenshields <kgreensh@ucsd.edu>
2015-02-12 23:17:24 -0800

    tritonhub1

---
+++ data.js
@@ -0,0 +1,57 @@
+
+	myFunction([
+		{
+			"name": "Acamazing",
+			"description": "Acamazing is a student-run co-ed a cappella group at UCSD comprised of undergraduate students, we are united by our love and passion for music and singing.",
+			"imageURL": "http://acamazing.ucsd.edu/gallery/explosion.jpg",
+			"URL": "acamazing.html",
+			"website": "http://acamazing.ucsd.edu",		
+			"president": "Briana Zimmers",
+			"vp": "Nathan Thai"
+		},
+		{
+			"name": "American Institute of Chemical Engineers",
+			"description": "The AIChE aims to promote the professional development of Chemical Engineer majors at UCSD and to build a community through social activities, career-building events, and service opportunites.",
+			"imageURL": "https://lh5.googleusercontent.com/-3WaOFTDXFaw/T8xxXCYsJJI/AAAAAAAABpg/uftLu-kpMsc/s640/DSC_0131.jpg",
+			"URL": "aiche.html",
+			"website": "http://aiche.ucsd.edu",
+			"president": "Bao Vo Ngo",
+			"vp": "Sarah Diaz/ Amy Chung"			
+		},
+		{
+			"name": "Anthropology Club",
+			"description": "The UCSD Anthropology Club is a loose coalition of students and faculty interested in all branches of anthropology. Meetings are casual and usually involve good food and friendly discussion.",
+			"imageURL": "https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xfa1/v/t1.0-9/27806_389549724365_3548489_n.jpg?oh=c3dcc735eacc29e142aded57f4013952&oe=5557FB5D&__gda__=1431711268_41823dd77a4b6016fe50e165b3623791",
+			"URL": "anthro.html",
+			"website": "http://anthroclub.ucsd.edu",
+			"president": "Sarah Mace",
+			"vp": "Althea Escalante"		
+		},
+		{
+			"name": "Boxing Club at UCSD",
+			"description": "To promote awareness and appreciation of the sport of boxing as well as supporting those who seek to compete at the intercollegiate league.",
+			"imageURL": "http://boxingatucsd.weebly.com/uploads/4/5/7/0/45700963/7985088_orig.jpg",
+			"URL": "boxing.html",
+			"website": "http://boxingatucsd.weebly.com",
+			"president": "Kurt Nyugen",
+			"vp": "Nathaneal Malazo"	
+		},
+		{
+			"name": "Circle K International",
+			"description": "Circle K Internation (CKI) is an organization at the collegiate and university levels. CKI promotes community service, leadership development, and fellowship and frienship.",
+			"imageURL": "https://c1.staticflickr.com/9/8531/8505099001_c0b1d24b85_z.jpg",
+			"URL": "cki.html",
+			"website" : "http://ucsdcki.org",
+			"president": "Joseph Le",
+			"vp": "Emilie Shen"
+		},
+		{
+			"name": "Cognitive Science Student Association",
+			"description": "We aim to connect with professional groups in the San Diego area in order to help our members discover the multidisciplinary qualifications that Cognitive Science offers to the professional world.  Our events, meetings, outings, and easy-going atmosphere promote a close-knit community and provide invaluable services and opportunities to anyone who is interested in becoming an active member of CSSA.",
+			"imageURL": "https://ucsdcssa.files.wordpress.com/2013/02/alex.jpg?w=300",
+			"URL": "cssa.html",
+			"website": "http://cssa.ucsd.edu/",
+			"president": "Nicholas Dudeck",
+			"vp": "Wednesday Bushong"
+		}
+	]);
